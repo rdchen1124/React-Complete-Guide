922f6ed386c6a26b81fbeffa92583ea4a4dcdbbf
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2021-12-20 18:01:46 +0800

    Use Card Component as wrapper of ExpenseItem & Expenses.

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,11 +1,12 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import './Expenses.css';
 
 const Expenses = ({items}) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {items.map(item => (<ExpenseItem key={item.id} {...item} />))}
-    </div>
+    </Card>
   )
 }
 
