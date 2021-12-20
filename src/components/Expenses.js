f82d43549076d7b7fe64e488a4a5d6c8c7ae4d68
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2021-12-20 16:48:37 +0800

    Let four ExpenseItem in App becomes to one Expenses Component, and use ExpenseItem to construct it.

---
+++ src/components/Expenses.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css';
+
+const Expenses = ({items}) => {
+  return (
+    <div className="expenses">
+      {items.map(item => (<ExpenseItem key={item.id} {...item} />))}
+    </div>
+  )
+}
+
+export default Expenses;
