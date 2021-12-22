3253e0b86573202bb23423d0d1b848a162404f70
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2021-12-22 17:10:14 +0800

    Adding yearFilter state and Passing handleYear not only for keeping it in Expenses, but using in ExpenseFilter.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,20 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import './Expenses.css';
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = ({items}) => {
+  const [yearFilter, setYearFilter] = useState('2020');
+  const handleYearFilterChange = (year) => {
+    setYearFilter(year);
+  }
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={yearFilter} onYearChange={handleYearFilterChange} />
       {items.map(item => (<ExpenseItem key={item.id} {...item} />))}
     </Card>
+
   )
 }
 
