59986e7178e046e0c7cf7857312419be3e0fbb1c
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2021-12-22 10:46:01 +0800

    Add NewExpense as a form for adding new user ExpenseItem.

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  }
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  }
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  }
+
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input 
+            type="text"
+            value={title}
+            onChange={handleTitleChange}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input 
+            type="number"
+            value={amount}
+            onChange={handleAmountChange}
+            min="0.01"
+            step="0.01"
+            />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            value={date}
+            onChange={handleDateChange}
+            min="2019-01-01"
+            max="2022-12-31"
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  )
+}
+
+export default ExpenseForm;
