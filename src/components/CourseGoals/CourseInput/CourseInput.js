3bd470644619c1af40b335f08c29210fce1b90fb
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2021-12-28 11:41:41 +0800

    Add isValid state for checking the submit value is valid.

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,13 +5,20 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
-
+  const [isValid, setIsValid] = useState(true);
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim(" ") > 0){
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim(" ").length === 0){
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
