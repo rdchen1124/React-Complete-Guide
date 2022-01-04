fc64e2597e1e78a2228a8e0151154f51fb2cc77c
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2022-01-04 15:17:39 +0800

    Use ReactDOM.createPortal to put ErrorModal and Backdrop at the place next to root.

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,13 +1,42 @@
 import React, {Fragment} from "react";
+import ReactDOM from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import styles from "./ErrorModal.module.css";
-
+const Backdrop = props => {
+  return (<div onClick={props.onConfirm} className={styles.backdrop} />);
+}
+const Overlay = props => {
+  return (
+    <Card cName={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  )
+}
 const ErrorModal = props => {
   return (
     <Fragment>
-      <div className={styles.backdrop} />
-      <Card cName={styles.modal}>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <Overlay 
+          title={props.title} 
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
+      {/* <Card cName={styles.modal}>
         <header className={styles.header}>
           <h2>{props.title}</h2>
         </header>
@@ -17,7 +46,7 @@ const ErrorModal = props => {
         <footer className={styles.actions}>
           <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
-      </Card>
+      </Card> */}
     </Fragment>
   )
 }
